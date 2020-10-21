25e9ed37197f336fe3946db8f2828e85fe87e0c6
shafiqrefat <shafiqrefat@gmail.com>
2020-10-21 23:49:31 +0600

    try to go fast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./components/Home/HomePage/HomePage";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Orders from "./components/Dashboard/Orders";
+import Review from "./components/Dashboard/Review";
+import ServiceList from "./components/Dashboard/ServiceList";
+import ServiceListAdmin from "./components/Dashboard/ServiceListAdmin";
+
+export const UserContext = createContext();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loggedInUser, setLoggedInUser] = useState({});
+	const globalStates = {
+		user: [loggedInUser, setLoggedInUser],
+	};
+
+	return (
+		<UserContext.Provider value={globalStates}>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<HomePage></HomePage>
+					</Route>
+					<Route path="/login">
+						<Login></Login>
+					</Route>
+					<PrivateRoute exact path="/dashboard">
+						<Dashboard></Dashboard>
+					</PrivateRoute>
+					<PrivateRoute exact path="/dashboard/orders">
+						<Orders></Orders>
+					</PrivateRoute>
+					<PrivateRoute exact path="/dashboard/orders/:_id">
+						<Orders></Orders>
+					</PrivateRoute>
+					<PrivateRoute exact path="/dashboard/service-list">
+						<ServiceList></ServiceList>
+					</PrivateRoute>
+					<PrivateRoute exact path="/dashboard/review">
+						<Review></Review>
+					</PrivateRoute>
+					<PrivateRoute exact path="/dashboard/service-list-admin">
+						<ServiceListAdmin></ServiceListAdmin>
+					</PrivateRoute>
+				</Switch>
+			</Router>
+		</UserContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/components/Dashboard/AddServices.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddServices = () => {
+    return (
+        <div>
+            <h3>add service</h3>
+        </div>
+    );
+};
+
+export default AddServices;
